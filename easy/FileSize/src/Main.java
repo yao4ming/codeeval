6d6b7f4c6592d2395e3622ee359783c40ebf39ec
Yaoming Jiang <yao4ming@gmail.com>
2016-01-18 15:29:57 -0500

    edited

--- easy/FileSize/src/Main.java
+++ easy/FileSize/src/Main.java
@@ -1,5 +1,10 @@
-/**
- * Created by hp on 1/18/2016.
- */
+import java.io.*;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        File file = new File(args[0]);
+        System.out.println(file.length());
+    }
 }
