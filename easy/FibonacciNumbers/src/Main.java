20163d9b026072be4f4f844a27f02708a6e64012
Yaoming Jiang <yao4ming@gmail.com>
2016-01-18 00:39:53 -0500

    initial commit

---
+++ easy/FibonacciNumbers/src/Main.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+
+    static int[] cache;
+
+    public static void main(String[] args) throws IOException {
+
+        File file = new File(args[0]);
+
+        BufferedReader in = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int n = Integer.parseInt(line);
+            cache = new int[n+1];
+            System.out.println(fib(n));
+        }
+    }
+
+    public static int fib(int n) {
+
+        if (n <= 1) return n;
+
+        if (cache[n] != 0) return cache[n];
+
+        cache[n] = fib(n-1) + fib(n-2);
+
+        return cache[n];
+    }
+}
